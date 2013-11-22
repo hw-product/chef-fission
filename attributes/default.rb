3ed0c9ce1148cb731666624accfa90e7b325e0de
Fletcher Nichol <fnichol@nichol.ca>
2013-11-22 13:27:25 -0700

    First pass at deploying the Nellie Jar from a URL.

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+default[:nellie][:pkg_url] = "node[:nellie][:pkg][:url] must be set"
+default[:nellie][:jar_path] = "/opt/nellie/nellie.jar"
+
+default[:nellie][:java_version] = 7
+
+default[:nellie][:user] = "nellie"
+default[:nellie][:group] = "nellie"
+default[:nellie][:java_path] = "/usr/bin/java"
+default[:nellie][:java_options] = "-Xms1024M -Xmx1024M"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,41 @@
+node.default[:java][:jdk_version] = node[:nellie][:java_version]
+
+jar_path = node[:nellie][:jar_path]
+
+if platform_family?("debian")
+  node.default[:java][:java_home] = "/usr/lib/jvm/default-java"
+  node.default[:java][:openjdk_packages] = [
+    "openjdk-#{node[:java][:jdk_version]}-jdk",
+    "openjdk-#{node[:java][:jdk_version]}-jre-headless"
+  ]
+end
+
 include_recipe "apt" if platform_family?("debian")
 include_recipe "java"
 include_recipe "runit"
+
+directory ::File.dirname(jar_path) do
+  recursive true
+end
+
+user node[:nellie][:user]
+group node[:nellie][:group]
+
+remote_file jar_path do
+  source node[:nellie][:pkg_url]
+  owner node[:nellie][:user]
+  group node[:nellie][:group]
+  mode "0644"
+  notifies :restart, "runit_service[nellie]"
+end
+
+runit_service "nellie" do
+  options({
+    :user => node[:nellie][:user],
+    :group => node[:nellie][:group],
+    :java_path => node[:nellie][:java_path],
+    :java_options => node[:nellie][:java_options],
+    :jar_path => jar_path
+  })
+  notifies :restart, "runit_service[nellie]"
+end
