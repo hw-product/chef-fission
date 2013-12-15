59fa01b0663f2b9c6e62ae59f141962503efd918
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-15 08:12:37 -0800

    Include attribute driven fission resource creation

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,6 @@ default[:fission][:name] = 'fission'
 default[:fission][:pkg_url] = "node[:fission][:pkg_url] must be set"
 default[:fission][:directories][:install] = '/opt'
 default[:fission][:directories][:config] = '/etc/fission'
-default[:fission][:config] = {}
 default[:fission][:java_version] = 7
 
 default[:fission][:user] = 'fission'
@@ -13,21 +12,4 @@ default[:fission][:java_options] = [
   '-Xmx1024M'
 ]
 
-
-
-default[:fission][:jar_path] = "/opt/fission/fission.jar"
-default[:fission][:current_jar] = File.basename(node[:fission][:pkg_url])
-default[:fission][:config_file] = "/etc/fission/config.json"
-default[:fission][:config][:options] = Hash.new
-
-default[:fission][:java_version] = 7
-
-default[:fission][:runit][:log_dir] = "/var/log/fission"
-
-default[:fission][:user] = "fission"
-default[:fission][:group] = "fission"
-default[:fission][:java_path] = "/usr/bin/java"
-default[:fission][:java_options] = [
-  "-Xms1024M",
-  "-Xmx1024M"
-]
+default[:fission][:instances] = {}
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,11 @@
 include_recipe 'fission::setup'
 
 # Loop attribute driven LWRP creation
+
+node[:fission][:instances].each do |fission_name, fission_opts|
+  fission fission_name do
+    fission_opts.each do |attribute_name, attribute_value|
+      self.send(attribute_name, attribute_value)
+    end
+  end
+end
