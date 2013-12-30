2c9e06c7f90476b06642cff820d430655bebd597
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-30 07:07:05 -0800

    Add attribute driven resource creation for web apps

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,19 @@
 include_recipe 'fission::setup'
 
-# Loop attribute driven LWRP creation
+# Loop attribute driven fission LWRP creation
 
 node[:fission][:instances].each do |fission_name, fission_opts|
   fission fission_name do
+    fission_opts.each do |attribute_name, attribute_value|
+      self.send(attribute_name, attribute_value)
+    end
+  end
+end
+
+# Loop attribute driven fission web app LWRP creation
+
+node[:fission][:web][:instances].each do |fission_name, fission_opts|
+  fission_web_app fission_name do
     fission_opts.each do |attribute_name, attribute_value|
       self.send(attribute_name, attribute_value)
     end
