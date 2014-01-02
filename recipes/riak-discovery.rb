015b90386da5c51435f0f60a98083897a5f09b8c
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-02 08:00:47 -0800

    Make nodes easy to override. Lazy load in the content.

--- recipes/riak-discovery.rb
+++ recipes/riak-discovery.rb
@@ -1,5 +1,9 @@
 
-node.default[:fission][:data_store][:riak][:nodes] = search(:node, "recipes:riak AND fission_core_group:#{node.fission.core.group.gsub(':', '\:')}")
+node.default[:fission][:data_store][:riak][:nodes] = search(:node,
+  "recipes:riak AND fission_core_group:#{node.fission.core.group.gsub(':', '\:')}"
+).map do |riak_node|
+  [:host => riak_node.ipaddress]
+end
 
 =begin
 discovery_all(
@@ -15,12 +19,10 @@ directory '/etc/fission' do
 end
 
 file '/etc/fission/riak.json' do
-  content(
+  content lazy{
     Chef::JSONCompat.to_json_pretty(
-      :nodes => node[:fission][:data_store][:riak][:nodes].map{|n|
-        [:host => n.ipaddress]
-      }
+      :nodes => node[:fission][:data_store][:riak][:nodes]
     )
-  )
+  }
   mode 0644
 end
