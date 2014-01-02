1cfe05c5a044f24d9aeeb5ba14ea4faa1f31dc5b
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-02 09:14:05 -0800

    Lets use valid syntax since we plan on actually using this file

--- recipes/riak-discovery.rb
+++ recipes/riak-discovery.rb
@@ -2,7 +2,7 @@
 node.default[:fission][:data_store][:riak][:nodes] = search(:node,
   "recipes:riak AND fission_core_group:#{node.fission.core.group.gsub(':', '\:')}"
 ).map do |riak_node|
-  :host => riak_node.ipaddress
+  {:host => riak_node.ipaddress}
 end
 
 =begin
