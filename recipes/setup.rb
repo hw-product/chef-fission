27058e791cef92d069977e6dc390caf685aea5f0
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-01 16:47:41 -0800

    Include java version set prior to loading the java resources

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,3 +1,5 @@
+node.set[:java][:jdk_version] = node[:fission][:java_version]
+
 if platform_family?("debian")
   node.default[:java][:java_home] = "/usr/lib/jvm/default-java"
   node.default[:java][:openjdk_packages] = [
