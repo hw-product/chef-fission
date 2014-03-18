4d4627583fded6c169717e1bd4b3b9c502cb0297
Chris Roberts <chrisroberts.code@gmail.com>
2014-03-17 18:54:31 -0700

    Include some extra initialization commands to the precise container

--- recipes/packager.rb
+++ recipes/packager.rb
@@ -1,3 +1,11 @@
 node.set[:vagabond][:disable_default_zero] = true
 
 include_recipe 'vagabond'
+
+begin
+  lxc_precise = node.run_context.resource_collection.lookup('lxc[ubuntu_1204]')
+  lxc_precise.initialize_commands << '/opt/chef/embedded/bin/gem install attribute_struct --no-rdoc --no-ri'
+  lxc_precise.initialize_commands << 'apt-get install lxc --no-install-recommends'
+rescue => e
+  Chef::Log.warn "Failed to customize Packager lxc install: #{e.class}: #{e}"
+end
