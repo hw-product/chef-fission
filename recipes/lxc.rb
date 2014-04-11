143d674aced4067215cd603ef0e278eed1afca1d
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-11 06:11:41 -0700

    Stub in default env var file and force apt to keep it

--- recipes/lxc.rb
+++ recipes/lxc.rb
@@ -4,8 +4,9 @@ include_recipe 'vagabond'
 
 begin
   lxc_precise = node.run_context.resource_collection.lookup('lxc[ubuntu_1204]')
+  lxc_precise.initialize_commands << 'cat "" > /etc/default/lxc'
   lxc_precise.initialize_commands << '/opt/chef/embedded/bin/gem install attribute_struct --no-rdoc --no-ri'
-  lxc_precise.initialize_commands << 'apt-get install lxc --no-install-recommends'
+  lxc_precise.initialize_commands << 'apt-get install lxc -o Dpkg::Options::="--force-confold" --no-install-recommends'
 rescue => e
   Chef::Log.warn "Failed to customize Packager lxc install: #{e.class}: #{e}"
 end
