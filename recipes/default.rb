3b10ccc5b61acdc7dfa879f5e1358a5a190afc69
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-04 15:07:33 -0800

    Proxy app installation to jackal unless pkg_url is provided (signaling a jar installation)

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,9 +3,19 @@ include_recipe 'fission::setup'
 # Loop attribute driven fission LWRP creation
 
 node[:fission][:instances].each do |fission_name, fission_opts|
+
+  # If we have a pkg_url then we know we have a jar build to configure
+  # and run. However, we encounter no pkg_url it is assumed a gem
+  # install and will install gems and proxy to jackal cookbook
   fission fission_name do
-    fission_opts.each do |attribute_name, attribute_value|
-      self.send(attribute_name, attribute_value)
+    if(fission_opts[:pkg_url])
+      fission_opts.each do |attribute_name, attribute_value|
+        self.send(attribute_name, attribute_value)
+      end
+    else
+      include_recipe 'fission::fission-gems'
+      include_recipe 'jackal'
+      node.set[:jackal][:apps][fission_name] = fission_opts
     end
   end
 end
