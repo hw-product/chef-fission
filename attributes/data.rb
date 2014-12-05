92e93c46da441688430ec270840f262c84f9427d
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-05 07:51:49 -0800

    Set postgres user directly. Is this detectable some where? Thought it was in pg cookbook but not finding it.

--- attributes/data.rb
+++ attributes/data.rb
@@ -2,5 +2,5 @@ default[:fission][:data][:stores] = {}
 default[:fission][:data][:discovery] = ['riak', 'sql']
 default[:fission][:data][:riak][:address_attribute] = 'ipaddress'
 default[:fission][:data][:sql][:address_attribute] = 'ipaddress'
-default[:fission][:data][:sql][:system_user] = node[:postgresql][:user]
+default[:fission][:data][:sql][:system_user] = 'postgres'
 default[:fission][:data][:sql][:credentials_data_bag] = "credentials"
