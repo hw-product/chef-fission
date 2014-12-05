9bdd30db9d643cee69201243a03e6a110cfc2878
Anthony Goddard <anthony@anthonygoddard.com>
2014-12-04 20:45:24 -0700

    add default databag name

--- attributes/data.rb
+++ attributes/data.rb
@@ -3,3 +3,4 @@ default[:fission][:data][:discovery] = ['riak', 'sql']
 default[:fission][:data][:riak][:address_attribute] = 'ipaddress'
 default[:fission][:data][:sql][:address_attribute] = 'ipaddress'
 default[:fission][:data][:sql][:system_user] = node[:postgresql][:user]
+default[:fission][:data][:sql][:credentials_data_bag] = "credentials"
