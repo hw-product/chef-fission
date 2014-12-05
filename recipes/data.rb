00989fe2c2157a1f9e169c53ba10eb4acd17bd4f
Anthony Goddard <anthony@anthonygoddard.com>
2014-12-04 20:46:40 -0700

    use new abstracted resource for fission_data

--- recipes/data.rb
+++ recipes/data.rb
@@ -1,6 +1,5 @@
-
 node[:fission][:data][:stores].each do |name, args|
-  fission_data_riak name do
+  fission_data name do
     args.each do |attr_name, attr_value|
       self.send(attr_name, attr_value)
     end
