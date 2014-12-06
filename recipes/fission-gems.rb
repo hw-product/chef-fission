87876a1589d086318c6d9e494f4f91b485a9737a
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-06 09:54:12 -0800

    Ensure build essential, nokogiri reqs and support rubygems on 1.9 and 2

--- recipes/fission-gems.rb
+++ recipes/fission-gems.rb
@@ -1,9 +1,12 @@
 # Install fission gems from private gemstore
 
-require 'rubygems/remote_fetcher'
-require 'rubygems/name_tuple'
+include_recipe 'build-essential'
 
-gemstore = discovery(
+['libxslt1-dev', 'libxml2-dev', 'zlib1g-dev'].each do |pkg|
+  package pkg
+end
+
+gemstore = discovery_search(
   'gemstore',
   :environment_aware => true,
   :empty_ok => false
@@ -11,9 +14,25 @@ gemstore = discovery(
 
 node.default[:fission][:private_gem_store] = "https://#{gemstore[:geminabox][:auth_required].to_a.first.join(':')}@#{gemstore.ipaddress}"
 
-Gem::Source.new(node[:fission][:private_gem_store]).load_specs(:released).find_all do |spec|
-  spec.name.start_with?('fission')
-end.map(&:name).uniq.each do |gem_name|
+begin
+  require 'rubygems/remote_fetcher'
+  require 'rubygems/name_tuple'
+
+  list = Gem::Source.new(node[:fission][:private_gem_store]).load_specs(:released).find_all do |spec|
+    spec.name.start_with?('fission')
+  end.map(&:name).uniq
+
+rescue LoadError
+
+  list = Gem::SpecFetcher.new.load_specs(
+    URI.parse(node[:fission][:private_gem_store]), :specs
+  ).find_all do |spec|
+    spec.first.start_with?('fission')
+  end.map(&:first).uniq
+
+end
+
+list.each do |gem_name|
 
   gem_package gem_name do
     action :install
