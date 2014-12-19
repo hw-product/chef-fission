27586151759204e040441a3b2ae7d0938cf46084
Anthony Goddard <anthony@anthonygoddard.com>
2014-12-19 08:57:16 -0700

    initial implemetation of lot_rotations

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,6 +34,9 @@ node[:fission][:web][:instances].each do |fission_name, fission_opts|
   end
 end
 
+# logrotate all the things
+include_recipe 'log_rotations'
+
 # enable nginx proxy to redirect HTTP->HTTPS
 if node[:fission][:web][:redirect_http] == true
   include_recipe 'nginx'
