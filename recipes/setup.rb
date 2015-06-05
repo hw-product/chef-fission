e199a4b76694446b78b6698fe7e5444486fb752d
Chris Roberts <code@chrisroberts.org>
2015-06-05 15:51:58 -0700

    Include auth info for download and store in run state

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,12 +5,19 @@ end
 token = credentials_for(:github)[:token]
 if(token)
   chef_gem 'octokit'
+  require 'uri'
   require 'octokit'
   client = Octokit::Client.new(:access_token => token)
-  node.set[:fission][:pkg_url] = client.releases('hw-product/fission-packaging').sort_by(&:created_at).last.assets.detect do |asset|
+  fission_app = client.releases('hw-product/fission-packaging').sort_by(&:created_at).last.assets.detect do |asset|
     asset.name.end_with?('.deb')
   end.url
-  node.set[:fission][:web][:pkg_url] = client.releases('hw-product/fission-app').sort_by(&:created_at).last.assets.detect do |asset|
+  fission_app = URI.parse(fission_app)
+  fission_app.userinfo = token
+  node.run_state[:fission_pkg_url] = fission_app.to_s
+  fission_web_app = client.releases('hw-product/fission-app').sort_by(&:created_at).last.assets.detect do |asset|
     asset.name.end_with?('.deb')
   end.url
+  fission_web_app = URI.parse(fission_web_app)
+  fission_web_app.userinfo = token
+  node.run_state[:fission_web_pkg_url] = fission_web_app.to_s
 end
