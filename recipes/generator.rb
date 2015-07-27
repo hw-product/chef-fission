b2b7dd5528edfe76778076c0b3caab8b84f24483
Chris Roberts <code@chrisroberts.org>
2015-07-27 13:34:05 -0700

    resource attribute setting is via method not setter

--- recipes/generator.rb
+++ recipes/generator.rb
@@ -29,7 +29,7 @@ reaper_config = credentials_for(:reaper_man)
 fission_service = resources('fission[pipeline]')
 if(fission_service)
   Chef::Log.info 'Added custom ReaperMan environment configuration to pipeline service'
-  fission_service.environment = reaper_config[:env]
+  fission_service.environment reaper_config[:env]
 else
   Chef::Log.warn 'Failed to locate pipeline service for ReaperMan environment configuration!'
 end
