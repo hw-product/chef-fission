fc223849f16d6fb189ced184bf8bbbe070085cc9
Chris Roberts <code@chrisroberts.org>
2015-10-08 06:53:04 -0700

    Set attributes correctly and batch the ssl generation

--- recipes/lxd-client.rb
+++ recipes/lxd-client.rb
@@ -5,7 +5,7 @@ directory '/etc/fission/ssl' do
 end
 
 execute 'create LXD key and certificate' do
-  command 'openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout /etc/fission/ssl/lxd.key -out /etc/fission/ssl/lxd.crt'
+  command 'openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout /etc/fission/ssl/lxd.key -out /etc/fission/ssl/lxd.crt -batch'
   creates '/etc/fission/ssl/lxd.key'
 end
 
@@ -21,7 +21,7 @@ lxd_node = search(:node, "roles:lxd").first
 
 if(lxd_node)
   node[:fission][:instances].each do |fission_name, fission_opts|
-    fission_opts[:configuration][:fission][:remote_process] = Mash.new(
+    fission_opts.default[:configuration][:fission][:remote_process] = Mash.new(
       :api_endpoint => "https://#{lxd_node.ipaddress}:8443",
       :password => node[:stack][:grouping],
       :ssl_key => '/etc/fission/ssl/lxd.key',
