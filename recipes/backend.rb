1765fac9bd00b81d22ffa2d4e84978c1104309dd
Chris Roberts <code@chrisroberts.org>
2016-03-30 14:55:56 -0700

    Add generic backend setup recipe

---
+++ recipes/backend.rb
@@ -0,0 +1,53 @@
+user node[:fission][:user] do
+  system true
+  home node[:fission][:home]
+end
+
+directory node[:fission][:home] do
+  recursive true
+  owner node[:fission][:user]
+end
+
+directory '/etc/fission' do
+  mode 0755
+end
+
+file '/etc/fission/backend.json' do
+  content Chef::JSONCompat.to_json_pretty(
+    Chef::Mixin::DeepMerge.merge(
+      node[:fission][:default_config].fetch(:service, {}),
+      node[:fission][:service][:config]
+    )
+  ).gsub('$NODE_NAME$', node.name)
+  mode 0644
+  notifies :restart, 'runit_service[fission]' if File.exists?('/etc/init.d/fission')
+end
+
+remote_file File.join(Chef::Config[:file_cache_path], 'fission-backend.deb') do
+  source node[:fission][:service][:asset_url]
+  mode 0644
+  notifies :install, 'dpkg_package[fission]', :immediately
+end
+
+dpkg_package 'fission' do
+  source File.join(Chef::Config[:file_cache_path], 'fission-backend.deb')
+  action :nothing
+  notifies :restart, 'runit_service[fission]' if File.exists?('/etc/init.d/fission')
+end
+
+runit_service 'fission' do
+  run_template_name 'fission-ruby'
+  options(
+    :user => node[:fission][:user],
+    :group => node[:fission][:user],
+    :script_path => '/usr/local/fission/bin/fission',
+    :config_file => '/etc/fission'
+  )
+  env(
+    {'FISSION_APPLICATION_NAME' => 'fission'}.merge(
+      node[:fission][:service][:env]
+    )
+  )
+  restart_on_update true
+  default_logger true
+end
