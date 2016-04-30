444f7066b620e84602e72ff77f6d43b9ac65bd26
Chris Roberts <code@chrisroberts.org>
2016-04-30 06:40:12 -0700

    Reduce enabled images for now

--- attributes/lxd.rb
+++ attributes/lxd.rb
@@ -1,12 +1,10 @@
 default[:fission][:lxd][:images] = {
-  :centos_6 => 'centos/6/amd64',
+#  :centos_6 => 'centos/6/amd64',
   :centos_7 => 'centos/7/amd64',
-  :debian_6 => 'debian/squeeze/amd64',
-  :debian_7 => 'debian/wheezy/amd64',
+#  :debian_7 => 'debian/wheezy/amd64',
   :debian_8 => 'debian/jessie/amd64',
-  :ubuntu_1204 => 'ubuntu/precise/amd64',
-  :ubuntu_1404 => 'ubuntu/trusty/amd64',
-  :ubuntu_1504 => 'ubuntu/vivid/amd64'
+#  :ubuntu_1204 => 'ubuntu/precise/amd64',
+  :ubuntu_1404 => 'ubuntu/trusty/amd64'
 }
 default[:fission][:lxd][:packages][:centos] = ['autoconf', 'bison', 'flex', 'make', 'gcc', 'gcc-c++', 'kernel-devel', 'm4', 'patch', 'curl', 'unzip', 'zip']
 default[:fission][:lxd][:packages][:default] = ['build-essential', 'curl', 'zip', 'unzip']
