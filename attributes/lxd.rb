b7458b83a097c7babfc8252292a494f2210b3547
Chris Roberts <code@chrisroberts.org>
2016-05-05 21:23:13 -0700

    Disable until bintray issues can be debugged

--- attributes/lxd.rb
+++ attributes/lxd.rb
@@ -6,14 +6,14 @@ default[:fission][:lxd][:images] = {
  # :opensuse_132 => 'opensuse/13.2/amd64',
  # :oracle_6 => 'oracle/6/amd64',
  # :oracle_7 => 'oracle/7/amd64',
- :debian_7 => 'debian/wheezy/amd64',
- :debian_8 => 'debian/jessie/amd64',
- :debian_testing => 'debian/stretch/amd64',
- :debian_unstable => 'debian/sid/amd64',
- :ubuntu_1204 => 'ubuntu/precise/amd64',
+ # :debian_7 => 'debian/wheezy/amd64',
+ # :debian_8 => 'debian/jessie/amd64',
+ # :debian_testing => 'debian/stretch/amd64',
+ # :debian_unstable => 'debian/sid/amd64',
+ # :ubuntu_1204 => 'ubuntu/precise/amd64',
  :ubuntu_1404 => 'ubuntu/trusty/amd64',
  :ubuntu_1604 => 'ubuntu/xenial/amd64',
- :ubuntu_1610 => 'ubuntu/yakkety/amd64'
+ # :ubuntu_1610 => 'ubuntu/yakkety/amd64'
 
 }
 default[:fission][:lxd][:packages][:centos] = ['autoconf', 'bison', 'flex', 'make', 'gcc', 'gcc-c++', 'kernel-devel', 'm4', 'patch', 'curl', 'unzip', 'zip']
