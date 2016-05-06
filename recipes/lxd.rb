72236a2c0bc666c8cd90086985c0a38b61d9fa5e
Chris Roberts <code@chrisroberts.org>
2016-05-06 06:30:39 -0700

    Stop all services before configuration. Find image tarball and set alias after import.

--- recipes/lxd.rb
+++ recipes/lxd.rb
@@ -4,9 +4,27 @@ lxd_config = node[:fission][:lxd][:config].map do |k,v|
   "--#{k.to_s.tr('_', '-')} #{v}"
 end.compact.join(' ')
 
+service 'lxd' do
+  action :stop
+  not_if{ File.exist?('/opt/.lxd-config-touch') }
+end
+
+service 'lxd-bridge' do
+  action :stop
+  not_if{ File.exist?('/opt/.lxd-config-touch') }
+end
+
 execute 'lxd configuration' do
   command "lxd --auto #{lxd_config}"
   not_if File.exists?('/opt/.lxd-config-touch')
+end
+
+service 'lxd' do
+  action :start
+end
+
+service 'lxd-bridge' do
+  action :start
 end
 
 file '/opt/.lxd-config-touch'
@@ -14,10 +32,15 @@ file '/opt/.lxd-config-touch'
 Dir.glob(File.join(node[:fission][:lxd][:image_directory], '*')).each do |image_path|
   next unless File.directory?(image_path)
   ctn_name = File.basename(image_path)
+  tarball = File.basename(Dir.glob(File.join(image_path, '*.tar.gz')).first)
 
-  execute "import container - #{ctn_name}" do
-    command "lxc image import metadata.tar.gz rootfs.tar.gz"
+  execute "import container - #{image_path}" do
+    command "lxc image import #{tarball}"
     cwd image_path
+  end
+
+  execute "alias imported container - #{ctn_name}" do
+    command "lxc image alias create #{ctn_name} #{tarball.slice(0, 12)}"
   end
 
   directory image_path do
